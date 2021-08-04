f95668d1f2367c28aa749e48ccc7cd093a9cbe9a
alexander-karle <alexander.karle@schibsted.com>
2021-08-04 19:50:14 +0400

    concatinate method

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
 import tablib
+import json
+
+urlPrefix = 'https://appgallery.cloud.huawei.com/ag/n/app/C27162'
 
 def main():
-    imported_data = importFile('input.csv')
-    parseCSV(imported_data)
-    
-def importFile(name):
+    importedCsv = importTable('input.csv')
+    importedJSON = open('map.json', 'r')
+    importedMap = json.load(importedJSON)
+    csvDict = CSVtoDict(importedCsv)
+    concatinated = concatinateDicts(importedMap, csvDict)
+    jsonDump = json.dumps(concatinated)
+    jsonFile = open("output.json", "w")
+    jsonFile.write(jsonDump)
+    jsonFile.close()
+
+def concatinateDicts(target, source):
+    result = target.copy()
+
+    for bundle, url in source.items():
+        url = url.strip()
+        if url == '':
+            result.pop(bundle, None)
+            continue
+        if bundle not in result:
+            result[bundle] = createURL(urlPrefix, url)
+            continue
+        result[bundle] = createURL(result[bundle], url)
+        
+
+    return result
+
+def createURL(oldURL, newURL):
+    oldURLWithoutID = oldURL[:oldURL.rindex('/')]
+    newID = newURL[newURL.rindex('/'):]
+    return oldURLWithoutID + newID
+
+def importTable(name):
     fh = open(name, 'r')
     return tablib.Dataset().load(fh)
 
-def parseCSV(data):
-    print(data[0])
-    print(data[1][0])
-    print(data['request.app.bundle'][0])
+def CSVtoDict(data):
+    d = dict()
+    for row in data:
+        bundle, url = row[1], row[4]
+        d[bundle] = url
+    return d
 
 main()
